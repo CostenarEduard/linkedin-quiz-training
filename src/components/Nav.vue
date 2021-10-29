7f076f34047dabb9ee6a5dffe0de70ad925698d9
Costenar Eduard <eduard@Costenars-MacBook-Pro.local>
2021-10-29 18:32:49 +0300

    wip

---
+++ src/components/Nav.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav class="nav">
+    <div class="container">
+      <button class="logo">Linkedin quiz training</button>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Nav"
+}
+</script>
+
+<style lang="scss" scoped>
+ .nav {
+   display: flex;
+   justify-content: center;
+   .container {
+     .logo {
+       background: #0b66c2;
+       color: white;
+       border: none;
+       font-size: 22px;
+       border-radius: 3px;
+       max-width: 132px;
+     }
+   }
+ }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,17 @@
 <template>
-  <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Nav />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Nav from "../components/Nav";
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Nav
   }
 }
 </script>
